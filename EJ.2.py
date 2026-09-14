9004dd6c9808222b1b5e7c685e0bf9623aee03eb
Iago Fontenla Gago <dam1_alu09@inf.ald@a113-pc09.inf.ald>
2026-09-14 13:53:13 +0200

    ejercicio2

---
+++ EJ.2.py
@@ -0,0 +1,6 @@
+numero1 = int(input("ingrese un número: "))
+numero2 = int(input("ingrese un número: "))  
+
+print("Division Real", numero1 / numero2)
+print("Division Entera", numero1 // numero2)
+print("Resto", numero1 % numero2)
