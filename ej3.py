dd9789fc487bab92f59d86216ae852e226363e71
Iago Fontenla Gago <dam1_alu09@inf.ald@a113-pc09.inf.ald>
2026-09-14 13:55:37 +0200

    ejercicio3

---
+++ ej3.py
@@ -0,0 +1,4 @@
+Nombre = input("Introduce tu nombre: ")
+Apellido1 = input("Introduce tu primer apellido: ")
+Apellido2 = input("Introduce tu segundo apellido: ")        
+print(f"El usuario {Apellido1} {Apellido2}, {Nombre} se ha registrado correctamente")
