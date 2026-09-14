836db474084dea2644ccf3e0e8c232104d2449b0
Iago Fontenla Gago <dam1_alu09@inf.ald@a113-pc09.inf.ald>
2026-09-14 14:06:11 +0200

    ejercicio7

---
+++ ej7.py
@@ -0,0 +1,3 @@
+nsegundos = int(input("Introduce el número de segundos: "))
+print("Equivalente en horas: ", nsegundos // 3600)
+print("Equivalente en minutos y segundos: ", nsegundos // 60, ":", nsegundos % 60)
