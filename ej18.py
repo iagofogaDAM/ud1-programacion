f5dd86fecf919d2013f1624e43862db02646df67
Iago <a26iagofg@iesantonlosada.gal>
2026-09-21 13:31:50 +0200

    ej17

---
+++ ej18.py
@@ -0,0 +1,8 @@
+cant1 = float(input("cuanto paga el primero"))
+cant2 = float(input("cuanto paga el segundo"))
+cant3 = float(input("cuanto paga el terecero"))
+
+x = cant1 + cant2 + cant3
+x /= 3 
+
+print (x)
