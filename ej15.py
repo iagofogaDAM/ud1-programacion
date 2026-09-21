42dd0484bf9c1d4517cceb65fb58f4ec6c9529f1
Iago <a26iagofg@iesantonlosada.gal>
2026-09-21 12:30:11 +0200

    ej15

---
+++ ej15.py
@@ -0,0 +1,8 @@
+distancia = float(input("Distancia en metros: "))
+distancia_en_metros = (distancia * 1000)
+millas = (distancia * 0.6213712)
+pies = (distancia * 3280.84)
+
+print (distancia_en_metros, "metros")
+print (millas, "millas")
+print (pies, "pies")
---
+++ ej16.py
@@ -0,0 +1,6 @@
+nota_tareas = int(input("Dame al nota de tus tareas"))
+nota_teorico = int(input("Dame al nota de tus examenes teoricos"))
+nota_practico = int(input("Dame al nota de tus examenes prácticos"))
+
+
+print ((nota_practico * 0.15)+(nota_teorico * 0.2)+(nota_practico * 0.65))
