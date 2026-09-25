9bcae31ca6c842e3ca7b2ff7cb24351e5fa592c3
Iago <a26iagofg@iesantonlosada.gal>
2026-09-25 12:50:01 +0200

    ej8 refresh

--- Ejercicios/bloque103/ej8.py
+++ Ejercicios/bloque103/ej8.py
@@ -6,5 +6,7 @@ if a == b and a == c :
     print ("Es un triangulo equilatero")
 elif a == b or a == c or b==c :
     print ("Es un triangulo isosceles")
+elif a <= 0 or b <= 0 or c <=0 :
+    print ("No es un triangulo valido")
 else :
     print ("Es un triangulo escaleno")
